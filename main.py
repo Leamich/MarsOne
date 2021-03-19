d3058f29a9c594c16463448fab4d3fcb2bf256c2
Leamich <mishak2005med@mail.ru>
2021-03-19 22:58:46 +0400

    Регистрация астронавтов

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -42,6 +42,13 @@ def show_mars_and_promote():
             crossorigin="anonymous">
         <link rel="stylesheet"
             href="{url_for('static', filename='css/style.css')}">
+        <title>Выбирай Марс!</title>
+        
+        <style>
+            h1 {"{"}
+                color: red;
+            {"}"}
+        </style>
     </head>
     <body>
         <h1>Жди нас, Марс</h1>
@@ -64,6 +71,122 @@ def show_mars_and_promote():
             Присоединяйся!
         </div>
     </body>'''
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def register_unit():
+    if request.method == 'GET':
+        return f'''	<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossorigin="anonymous">
+        <link rel="stylesheet" href={url_for('static', filename='css/style.css')}>
+        <title>Отбор кандидатов</title>
+    </head>
+    <body>
+    <h1 align="center">Анкета претендента</h1>
+    <h3 align="center">на участие в миссии</h3>
+    <form class="login_form" method="post" enctype="multipart/form-data">
+        <div class="row g-2">
+            <div class="col-6">
+                <input type="text" class="form-control" placeholder="Фамилия">
+            </div>
+            <div class="col-6">
+                <input type="text" class="form-control" placeholder="Имя">
+            </div>
+            <div class="col-12">
+                <label>Какое у вас образование?</label>
+                <select class="form-select">
+                    <option>Начальное</option>
+                    <option selected>Среднее</option>
+                    <option>Высшее</option>
+                </select>
+            </div>
+            <div class="col-12">
+                <label>Какими профессиями вы владеете?</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec1" value="Инженер-исследователь"></input>
+                <label class="form-check-label" for="spec1">Инженер-исследователь</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec2" value="Пилот"></input>
+                <label class="form-check-label" for="spec2">Пилот</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec3" value="Строитель"></input>
+                <label class="form-check-label" for="spec3">Строитель</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec4" value="Метеоролог"></input>
+                <label class="form-check-label" for="spec4">Метеоролог</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec5" value="Инженер по жизнеобеспечению"></input>
+                <label class="form-check-label" for="spec5">Инженер по жизнеобеспечению</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec6" value="Инженер по радиацинной защите"></input>
+                <label class="form-check-label" for="spec6">Инженер по радиацинной защите</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec7" value="Врач"></input>
+                <label class="form-check-label" for="spec7">Врач</label>
+                <br>
+
+                <input class="form-check-input" type="checkbox" name="speciality"
+                    id="spec8" value="Экзобиолог"></input>
+                <label class="form-check-label" for="spec8">Экзобиолог</label>
+                <br>
+            </div>
+            <div class="col-12">
+                <label>Укажите ваш пол</label>
+                <br>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="male" value="male"></input>
+                    <label class="form-check-label" for="male">Мужской</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="female" value="female"></input>
+                    <label class="form-check-label" for="female">Женский</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <label>Почему вы хотите принять участие в миссии?</label>
+                <textarea cols=50></textarea>
+            </div>
+            <div class="col-12">
+                <label>Приложите фотографию</label>
+                <input type="file" accept="image/*"></input>
+            </div>
+            <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="yes" id="areyouready"></input>
+                <label class="form-check-label" for="areyouready">Готовы ли остаться на Марсе?</label>
+            </div>
+            </div> 
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Отправить</button>
+            </div>
+            </div>
+        </form>
+    </body>
+</html>'''
+    elif request.method == 'POST':
+        print(request.form['name'])
+        return 'Отправлено'
 
 
 if __name__ == '__main__':
