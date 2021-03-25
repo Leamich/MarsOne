421edfd73bac272c19a156776e18742b8c1f0a20
Leamich <tennantonelove@yandex.ru>
2021-03-25 19:46:47 +0400

    Работа с запросами (и там sqlalchemy)

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, url_for, request, render_template,\
     redirect
 
+import datetime as dt
+import os
+
+from sqlalchemy import or_
+
+from data.jobs import Jobs
+from data.users import User
+from data import db_session
+
+
 PROF_LIST = ('Инженер-исследователь',
              'Пилот', 'Строитель', 'Метеоролог',
              'Инженер по жизнеобеспечению',
@@ -9,12 +19,7 @@ PROF_LIST = ('Инженер-исследователь',
 
 answer = {}
 app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/index')
-def index_default():
-    return render_template('base.html', title='MarsOne')
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/<title>')
@@ -157,5 +162,102 @@ def room_distribute():
                            participants_list=participants_list)
 
 
+@app.route('/')
+@app.route('/index')
+def work_journal():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(jobs)
+
+    return render_template('works_journal.html', jobs=jobs)
+
+
+def create_user(**kwargs):
+    user = User()
+    for arg_name in kwargs.keys():
+        user.__setattr__(arg_name, kwargs[arg_name])
+    return user
+
+
+def create_job(**kwargs):
+    job = Jobs()
+    for arg_name in kwargs.keys():
+        job.__setattr__(arg_name, kwargs[arg_name])
+    return job
+
+
+def initiate_users(db_sess):
+    db_sess.add(create_user(**{'surname': 'Scott',
+                               'name': 'Ridley',
+                               'age': 21,
+                               'position': 'captain',
+                               'speciality': 'research engineer',
+                               'address': 'module_1',
+                               'email': 'scott_chief@mars.org'
+                               }))
+
+    db_sess.add(create_user(**{'surname': 'Stiles',
+                               'name': 'Richard',
+                               'age': 22,
+                               'position': 'helper',
+                               'speciality': 'general',
+                               'address': 'module_3',
+                               'email': 'stile_r@mars.org'
+                               }))
+
+    db_sess.add(create_user(**{'surname': 'Crowds',
+                               'name': 'Scott',
+                               'age': 23,
+                               'position': 'junior captain',
+                               'speciality': 'math',
+                               'address': 'module_2',
+                               'email': 'scotty_cr1998@mars.org'
+                               }))
+
+    db_sess.add(create_user(**{'surname': 'Canon',
+                               'name': 'Pictures',
+                               'age': 24,
+                               'position': 'operator',
+                               'speciality': 'cinema',
+                               'address': 'module_7',
+                               'email': 'canon_pict@mars.org'
+                               }))
+
+    db_sess.add(create_user(**{'surname': 'Ronan',
+                               'name': 'Warwar',
+                               'age': 25,
+                               'position': 'warior',
+                               'speciality': 'alien killer',
+                               'address': 'module_1',
+                               'email': 'kill_mars@mars.org'
+                               }))
+
+
+def initiate_jobs(db_sess):
+    db_sess.add(create_job(**{'team_leader': 1,
+                              'job': 'deployment of residential modules 1 '
+                                     'and 2',
+                              'work_size': 15,
+                              'collaborators': '2, 3',
+                              'start_date': dt.date.today(),
+                              'is_finished': False
+                              }))
+
+
+def main():
+    Jobs.__repr__ = lambda self: f'<Job> {self.job}'
+
+    db_session.global_init(input())
+
+    db_sess = db_session.create_session()
+
+    jobs = map()
+
+    '''initiate_users(db_sess)
+    initiate_jobs(db_sess)'''
+
+
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
